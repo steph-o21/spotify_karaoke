cdfcb6488d467a0e429d69e97d231e4d1ace0a35
Here2Dance <mroussell1@student.gsu.edu>
2021-07-20 21:01:42 -0400

    Added Spotify Remote and Auth BackEnd
    creating mroussell branch

--- app/src/main/java/com/example/spotifykaraokeee/MainActivity.java
+++ app/src/main/java/com/example/spotifykaraokeee/MainActivity.java
@@ -5,11 +5,23 @@ import androidx.core.app.ActivityOptionsCompat;
 
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.*;
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,9 +33,19 @@ public class MainActivity extends AppCompatActivity {
     //allows us to transition to next screen
     private Thread mSplashThread;
 
+    // Set up Spotify Credentials
+    private static final int REQUEST_CODE = 1337;
+    private static final String CLIENT_ID = "your_client_id";
+    private static final String REDIRECT_URI = "http://com.yourdomain.yourapp/callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        LogIn();
 
         //sets no title and makes splash screen into full screen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -61,5 +83,100 @@ public class MainActivity extends AppCompatActivity {
         };
         mSplashThread.start();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams, new Connector.ConnectionListener() {
+            @Override
+            public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                mSpotifyAppRemote = spotifyAppRemote;
+                Log.d("MainActivity", "Connected! Yay!");
+
+                // Now you can start interacting with App Remote
+                connected();
+
+            }
+
+            @Override
+            public void onFailure(Throwable error) {
+                // Something went wrong if you go here!
+                Log.e("MyActivity", error.getMessage(), error); // edited from throwable.getMessage()
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+
+    // returns activity result from request and intent
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
+
+    // Connected functionality for remote play
+    private void connected() {
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActively", track.name + "by + " + track.artist.name);
+                    }
+                });
+    }
+
+    // Set up Spotify Auth Logging in
+    private void LogIn() {
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID,
+                        AuthenticationResponse.Type.TOKEN,
+                        REDIRECT_URI);
+
+        builder.setScopes(new String[]{"streaming"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    // Set Up Spotify Auth Logging Out
+    private void LogOut() {
+
+    }
+
+
 
 }
---
+++ app/src/main/java/com/example/spotifykaraokeee/MySpotifyAuthenticationActivity.java
@@ -0,0 +1,6 @@
+package com.example.spotifykaraokeee;
+
+import android.app.Activity;
+// Needed for Auth Error handle
+public class MySpotifyAuthenticationActivity extends Activity {
+}
